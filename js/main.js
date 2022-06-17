d62657511c5808ff6a40d9c56bb0b76cc7481ed7
Александр <esarev@inbox.ru>
2022-06-17 20:17:10 +0700

    feat: list.js

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const lists = document.querySelectorAll('.list');
+
+function addTask() {
+    const btn = document.querySelector('.add__btn'),
+        addBtn = document.querySelector('.add__item-btn'),
+        cancelBtn = document.querySelector('.cancel__item-btn'),
+        textarea = document.querySelector('.textarea'),
+        form = document.querySelector('.form');
+
+        let value;
+
+        btn.addEventListener('click', () => {
+            form.style.display = 'block';
+            addBtn.style.display = 'none';
+            btn.style.display = 'none';
+
+            textarea.addEventListener('input', e => {
+                value = e.target.value;
+                if(value) {
+                    addBtn.style.display = 'block';
+                } else {
+                    addBtn.style.display = 'none';
+                }
+            });
+        });
+}
+addTask();
